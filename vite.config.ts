f7b483a86d3b56310372c8451586ff3b64841689
radbile2022 <wahradar511@gmail.com>
2025-09-07 08:23:12 +0700

    feat : enhance perform with setting rolups
    there is working, but file highlight has not decreased until best
    practicee 500kb, there is still 900kb

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-  return defineConfig({
-    plugins: [react()],
+  return {
+    plugins: [
+      react(),
+      visualizer({ open: true }),
+    ],
     server: {
       proxy: {
         "/api": {
@@ -16,9 +20,33 @@ export default ({ mode }: { mode: string }) => {
         },
       },
     },
-  });
-};
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react-quill')) return 'quill';
+              if (id.includes('highlight.js')) return 'highlight';
+              if (id.includes('react')) return 'react';
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
+  };
+});
 
 
 // npm install --save-dev @types/node
 // npx vite build --mode production
+
+/*
+*  HANDLE : this
+// npm install --save-dev rollup-plugin-visualizer
+
+(!) Some chunks are larger than 500 kB after minification. Consider:
+- Using dynamic import() to code-split the application
+- Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/configuration-options/#output-manualchunks
+- Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.
+*/
