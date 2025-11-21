92a31068d0f01117d7f414918b6c525c5c0b9d9f
radbile2022 <wahradar511@gmail.com>
2025-11-21 10:03:19 +0700

    fix - gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,52 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://minio-s3.radarku.online",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "./",
+    plugins: [react()],
+
+    server: {
+      host: true,
+      port: 3000,
+
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,   // pakai dari .env
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+
+        "/minio": {
+          target: env.VITE_MINIO_URL,      // pakai dari .env
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/minio/, ""),
+        },
       },
     },
-  },
-  
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+
+    build: {
+      chunkSizeWarningLimit: 1500, // optional, no error
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react')) return 'react';
+              if (id.includes('react-router')) return 'router';
+              if (id.includes('lucide-react')) return 'icons';
+              return 'vendor'; // untuk modul-modul lain
+            }
+          },
+        },
+      },
+    }
+  };
 });
+
 
 // npm install --save-dev @types/node
 // npx vite build --mode production
